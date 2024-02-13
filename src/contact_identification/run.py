246bc04e3506dfa7f91abc137b6502905ebd82e4
Boobesh24 <159907345+Boobesh24@users.noreply.github.com>
2024-02-13 19:58:22 +0530

    Update run.py

--- src/contact_identification/run.py
+++ src/contact_identification/run.py
@@ -2,6 +2,4 @@ from app import app
 import os
 
 if __name__ == '__main__':
-    if not os.path.exists('contacts.db'):
-        os.system('sqlite3 contacts.db < contacts.sql')
     app.run(debug=True)
